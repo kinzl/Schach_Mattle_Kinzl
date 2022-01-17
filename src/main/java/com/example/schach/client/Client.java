87076a5b7a5e7b4cfc9c7e05d8b4823182b04bc8
kinzl <kinzle190027@sus.htl-grieskirchen.at>
2022-01-17 12:24:41 +0100

    Server/Client startup chessfield

--- src/main/java/com/example/schach/client/Client.java
+++ src/main/java/com/example/schach/client/Client.java
@@ -5,18 +5,20 @@ import java.net.Socket;
 
 public class Client {
     private String IPADDRESS;
+    private boolean running = true;
 
     public Client(String IPADDRESS) {
         this.IPADDRESS = IPADDRESS;
     }
 
-    public static void main(String[] args) {
-        try(Socket socket = new Socket("localhost",22);
+    public void connectToServer() {
+        try(Socket socket = new Socket(IPADDRESS,22);
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));)
          {
 
-             
+             String s = br.readLine();
+             System.out.println(s);
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/example/schach/server/Server.java
+++ src/main/java/com/example/schach/server/Server.java
@@ -1,40 +1,53 @@
 package com.example.schach.server;
 
 import java.io.*;
+import java.net.Inet4Address;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Server {
     private static int PORT = 22;
-
-    public static void startServer() {
-        scanPort();
-        try(ServerSocket serverSocket = new ServerSocket(PORT);
-            Socket socket = serverSocket.accept();
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));)
-        {
+    private static boolean connected = false;
 
 
+    public void startServer() {
+        boolean running = true;
+        try {
+            System.out.println(Inet4Address.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        try (ServerSocket serverSocket = new ServerSocket(PORT);
+             Socket socket = serverSocket.accept();
+             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));) {
+            connected = true;
+
+
+                System.out.println(connected);
+
+                bw.write("This was written by the server");
 
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+
         }
     }
 
-    private static void scanPort(){
-        Scanner scanner = new Scanner(System.in);
-        boolean running = true;
-        while (running){
-            System.out.println("Please, type in your port: ");
-            try{
-                PORT = Integer.parseInt(scanner.nextLine());
-                running = false;
-            } catch (Exception e){
-                System.out.println("Unguilty input, please try again");
-            }
-        }
+    public static boolean isConnected() {
+        return connected;
+    }
 
+    public static String getIpAddress() {
+        try {
+
+            return Inet4Address.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
