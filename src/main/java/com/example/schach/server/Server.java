e39104fbc4e823b44ec522e3655d0861bbc95ce0
kinzl <kinzle190027@sus.htl-grieskirchen.at>
2022-01-17 08:21:05 +0100

    Server user Interface

--- src/main/java/com/example/schach/server/Server.java
+++ src/main/java/com/example/schach/server/Server.java
@@ -4,16 +4,33 @@ import com.example.schach.client.HelloController;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     private static int PORT;
 
     public static void main(String[] args) {
+        scanPort();
         try(ServerSocket socket = new ServerSocket(PORT);
         ) {
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void scanPort(){
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while (running){
+            System.out.println("Please, type in your port: ");
+            try{
+                PORT = Integer.parseInt(scanner.nextLine());
+                running = false;
+            } catch (Exception e){
+                System.out.println("Unguilty input, please try again");
+            }
+        }
+
     }
 }
