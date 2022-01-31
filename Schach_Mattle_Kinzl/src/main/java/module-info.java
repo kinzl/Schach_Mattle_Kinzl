f73db192f2704aa4089fefb11500b315a775a7d4
kinzl <kinzle190027@sus.htl-grieskirchen.at>
2022-01-31 11:17:12 +0100

    Client / Server are allowed to communicate

--- Schach_Mattle_Kinzl/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.schach {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.schach.client to javafx.fxml;
-    exports com.example.schach.client;
-}
