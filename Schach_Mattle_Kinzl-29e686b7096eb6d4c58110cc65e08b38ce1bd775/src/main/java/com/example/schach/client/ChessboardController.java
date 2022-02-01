2d1ca10358193072566991ffda8531ca1c327ed2
kinzl <kinzle190027@sus.htl-grieskirchen.at>
2022-02-01 14:32:38 +0100

    Overall Push

---
+++ Schach_Mattle_Kinzl-29e686b7096eb6d4c58110cc65e08b38ce1bd775/src/main/java/com/example/schach/client/ChessboardController.java
@@ -0,0 +1,144 @@
+package com.example.schach.client;
+
+import javafx.event.EventType;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import java.nio.channels.Pipe;
+
+public class ChessboardController {
+    public GridPane chessBoardView;
+    public ImageView black_left_horse;
+    public ImageView black_left_runner;
+    public ImageView black_queen;
+    public ImageView black_king;
+    public ImageView black_right_runner;
+    public ImageView black_right_horse;
+    public ImageView black_right_tower;
+    public ImageView white_bauer0_6;
+    public ImageView white_bauer1_6;
+    public ImageView white_bauer2_6;
+    public ImageView black_left_tower;
+    public ImageView white_bauer3_6;
+    public ImageView white_bauer4_6;
+    public ImageView white_bauer5_6;
+    public ImageView white_bauer6_6;
+    public ImageView white_bauer7_6;
+    public ImageView white_left_tower;
+    public ImageView white_left_horse;
+    public ImageView white_left_runner;
+    public ImageView white_queen;
+    public ImageView white_king;
+    public ImageView white_right_runner;
+    public ImageView white_right_horse;
+    public ImageView white_right_tower;
+    public ImageView black_bauer;
+    public ImageView img;
+    public Node node;
+    public Node checkField;
+    public Node slectedPic;
+    public Node clickedpic;
+    public ImageView black_bauer1;
+    public ImageView black_bauer2;
+    public ImageView black_bauer3;
+    public ImageView black_bauer4;
+    public ImageView black_bauer5;
+    public ImageView black_bauer6;
+    public ImageView black_bauer7;
+    public ImageView black_bauer8;
+    public boolean isChoosen = false;
+    public boolean pressed = false;
+    public static MouseEvent mouseEvent1;
+    public int isChoosenInt = 0;
+    public void fieldslected00(MouseEvent mouseEvent) {
+        mouseEvent1 = mouseEvent;
+
+        if (clickedpic ==  (Node) mouseEvent.getSource() ){
+            pressed = false;
+            isChoosen = true;
+            clickedpic = null;
+            System.out.println("Feld deaktiviert");
+        }else if(clickedpic != (Node) mouseEvent.getSource() && isChoosen == true && pressed == true){
+            slectedPic  = (Node) mouseEvent.getSource();
+            for (Node n: chessBoardView.getChildren()) {
+                if(n == slectedPic){
+                    n.setVisible(false);
+                }
+            }
+            Integer b = null;
+            Integer x = GridPane.getRowIndex((Node) slectedPic);
+            Integer y = GridPane.getColumnIndex((Node) slectedPic);
+            if(x == b){
+                x = 0;
+            }
+            if(y == b){
+                y = 0;
+            }
+            int []move = movePawn(x,y);
+            GridPane.setRowIndex(clickedpic,move[0]);
+            GridPane.setColumnIndex(clickedpic,move[1]);
+            isChoosen = false;
+            pressed = false;
+
+            System.out.println("Feld ersetzt");
+
+        }else
+        {
+            clickedpic = (ImageView) mouseEvent.getSource();
+            pressed = true;
+            isChoosen = true;
+            System.out.println("Feld ausgewählt");
+
+        }
+
+    }
+
+    public void mouseDragExited(MouseEvent mouseEvent2) {
+       Integer b = null;
+        Integer x = GridPane.getRowIndex((Node) mouseEvent2.getSource());
+        Integer y = GridPane.getColumnIndex((Node) mouseEvent2.getSource());
+        if(x == b){
+            x = 0;
+        }
+        if(y == b){
+            y = 0;
+        }
+        int []move = movePawn(x,y);
+        GridPane.setRowIndex(clickedpic,move[0]);
+        GridPane.setColumnIndex(clickedpic,move[1]);
+
+        System.out.println("Feld verschoben");
+            isChoosen = false;
+            pressed = false;
+        }
+
+    public int[] movePawn(int x, int y)
+    {
+        Integer newX = x;
+        Integer newY = y;
+        Integer oldX = GridPane.getRowIndex((Node) mouseEvent1.getSource());
+        Integer oldY = GridPane.getColumnIndex((Node) mouseEvent1.getSource());
+        String name = clickedpic.getId().replaceAll(".$","");
+
+        if(newX < oldX && name.contains("black"))
+        {
+            return new int[]{oldX, oldY};
+        }else if(newX > oldX && name.contains("white"))
+        {
+            return new int[]{oldX, oldY};
+        }else if(oldX== 6 && name.contains("white"))
+        {
+
+        }
+
+
+        return new int[]{newX, newY};
+
+    }
+
+}
---
+++ Schach_Mattle_Kinzl-29e686b7096eb6d4c58110cc65e08b38ce1bd775/src/main/java/com/example/schach/server/MyServerThread.java
@@ -0,0 +1,79 @@
+package com.example.schach.server;
+
+
+import java.io.*;
+import java.net.Socket;
+
+public class MyServerThread extends Thread {
+
+    private Socket socket = null;
+    private boolean running;
+    private static String username;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private static String clientUsername;
+
+    public static void setUsername(String username) {
+        MyServerThread.username = username;
+    }
+
+    public MyServerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            running = true;
+
+
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            handleUsername();
+            String s;
+
+            while (running) {
+                //s = reader.readLine();
+                //TODO: Connection to Server
+                System.out.println("Server received from Client: " + clientUsername);
+                running = false;
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    void stopServerTread() {
+        running = true;
+    }
+
+
+    private void handleUsername(){
+        try {
+            String s = reader.readLine();
+            if(s.equals("handleUsername")) {
+                clientUsername = reader.readLine();
+            }
+
+            writer.write("handleUsername");
+            writer.newLine();
+            writer.flush();
+            writer.write(username);
+            writer.newLine();
+            writer.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getClientUsername() {
+        return clientUsername;
+    }
+}
+
